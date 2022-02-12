a4f4228d34021e04cf7206e13f82a9038aafc043
Eirc <dongzihao123@126.com>
2022-02-11 16:03:33 -0800

    init commit

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-introduction',
@@ -6,10 +14,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.css']
 })
 export class IntroductionComponent implements OnInit {
+  @Output()
+  changeFile: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() { }
-
+  tiles: Tile[] = [
+    {text: 'one', cols: 6, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightpink'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightpink'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightpink'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+  ];
   ngOnInit(): void {
   }
 
+   changeEvent(num: number): void {
+    this.changeFile.emit(num);
+
+  }
 }

3c4496d0b592049b64cb70bfbf26038c2531cf8f
Eirc <dongzihao123@126.com>
2022-02-12 13:12:55 -0800

    init commit

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -14,17 +14,13 @@ export class UploadComponent implements OnInit {
 
   FeatureFile: File = new File([], '');
   LabelFile: File = new File([], '');
+  public enteredToken = '';
+  @Input()
+  token: string;
+  @Output()
+  changeToken: EventEmitter<string> = new EventEmitter<string>();
   @Output()
   changePage: EventEmitter<number> = new EventEmitter<number>();
-  @Input()
-  feature: string;
-  @Input()
-  label: string;
-  @Output()
-  featureToken: EventEmitter<string> = new EventEmitter<string>();
-
-  @Output()
-  labelToken: EventEmitter<string> = new EventEmitter<string>();
 
   code =
     '\tX_train # your feature numpy array\n' +
@@ -35,6 +31,7 @@ export class UploadComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.token);
   }
 
   uploadFile(Body: FormData): Observable<any> {
@@ -57,49 +54,39 @@ export class UploadComponent implements OnInit {
     });
   }
 
-  changeFile($event: any, type: string) {
+  changeFile($event: any, type: string): void {
     console.log(type);
     const Formdata = new FormData();
-    if (type == 'feature') {
-      this.FeatureFile = $event.target.files[0];
-      Formdata.append('file', $event.target.files[0]);
-      Formdata.append('fileType', 'feature');
-      this.uploadFile(Formdata).subscribe(
-        res => {
-          // @ts-ignore
-          const result = res.body.body.fileid;
-          console.log(result);
-          this.feature = result;
-          this.featureToken.emit(result);
-          this.snackBar.open('Upload Success', 'Close');
-        },
-        err => {
-          this.snackBar.open('Fail to Open Your File', 'RETRY');
-        }
-      );
-      console.log('feature' + this.FeatureFile.name);
-    } else if (type == 'label') {
-      this.LabelFile = $event.target.files[0];
-      Formdata.append('file', $event.target.files[0]);
-      Formdata.append('fileType', 'label');
-      this.uploadFile(Formdata).subscribe(
-        res => {
-          // @ts-ignore
-          const result = res.body.body.fileid;
-          console.log(result);
-          this.label = result;
-          this.labelToken.emit(result);
-          this.snackBar.open('Upload Success', 'close');
-        },
-        err => {
-          this.snackBar.open('Fail to Open Your File', 'RETRY');
-        }
-      );
+    Formdata.append('file', $event.target.files[0]);
+    Formdata.append('fileType', type);
+    console.log($event.target.files[0].filename);
+    if (this.token != undefined){
+      Formdata.append('token', this.token);
     }
-
+    this.uploadFile(Formdata).subscribe(
+      res => {
+        this.token = res.body.body.token;
+        this.changeToken.emit(this.token);
+        this.snackBar.open('Upload Success', 'Close');
+      }
+    );
   }
 
   getNoise($event: MouseEvent): void {
     this.changePage.emit(2);
+  }
+
+  enterToken($event: MouseEvent): void {
+    console.log(this.enteredToken);
+    const req = new HttpRequest('GET', 'http://127.0.0.1:5000/checktoken?token=' + this.enteredToken);
+    this.http.request(req).pipe(filter(e => e instanceof HttpResponse)).subscribe(
+      (rest: HttpResponse<any>) => {
+        console.log(rest);
+        if (rest.body.has_token){
+          this.token = this.enteredToken;
+          this.changeToken.emit(this.token);
+        }
+      },
+    );
   }
 }
